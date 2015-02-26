c3eabfc71bb4e2b6ca4dd1730ced26159bb156fa
Ilia Choly <ilia.choly@gmail.com>
2015-02-26 10:29:18 -0500

    steal angular cookie code

--- src/javascript/app/ng-biscuit/cookieStoreService.js
+++ src/javascript/app/ng-biscuit/cookieStoreService.js
@@ -1,78 +1,112 @@
-angular.module('ngBiscuit').
-  service('cookieStore', function($document) {
+(function () {
 
-    function hasItem(key) {
-      return (new RegExp("(?:^|;\\s*)" + encodeURIComponent(key).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=")).test($document[0].cookie);
+  var CookieStoreService = function ($document) {
+
+    var baseElement = $document[0].find('base');
+    var lastCookies = {};
+    var lastCookieString = '';
+    var defaultCookiePath = baseHref();
+
+    function baseHref() {
+      var href = baseElement.attr('href');
+      return href ? href.replace(/^(https?\:)?\/\/[^\/]*/, '') : '';
     }
 
-    function encodeValue(value) {
-      return encodeURIComponent(JSON.stringify(value));
+    function safeDecodeURIComponent(str) {
+      try {
+        return decodeURIComponent(str);
+      } catch (e) {
+        return str;
+      }
     }
 
-    function decodeValue(value) {
-      return JSON.parse(decodeURIComponent(value))
+    function encodeAttributes(options) {
+      var attr = "";
+      options = options || {};
+      options.path = options.path || defaultCookiePath;
+
+      if (angular.isDefined(options.domain)) {
+        attr += ";domain=" + options.domain;
+      }
+      if (angular.isDefined(options.path)) {
+        attr += ";path=" + options.path;
+      }
+      if (angular.isDefined(options.expires)) {
+        attr += ";expires=" + options.expires;
+      }
+      if (angular.isDefined(options.maxAge)) {
+        attr += ";max-age=" + options.maxAge;
+      }
+      if (options.secure === true) {
+        attr += ";secure"
+      }
+      return attr;
     }
 
-    return {
+    function encodeCookie(name, value, options) {
+      var encodeValue;
+      if (angular.isDefined(value)) {
+        encodeValue = encodeURIComponent(angular.toJson(value));
+      } else {
+        encodeValue = '';
+      }
+      return encodeURIComponent(name) + '=' + encodeValue + encodeAttributes(options):
+    }
 
-      get: function(key) {
-        var value = $document[0].cookie.replace(new RegExp("(?:(?:^|.*;)\\s*" + encodeURIComponent(key).replace(/[\-\.\+\*]/g, "\\$&") + "\\s*\\=\\s*([^;]*).*$)|^.*$"), "$1");
-        return decodeValue(value) || undefined;
-      },
+    this.remove = function (name, options) {
+      rawDocument.cookie = encodeCookie(name, undefined, options);
+    };
 
-      /**
-       * options:
-       *  - end (Infinity|String|Date)
-       *  - path
-       *  - domain
-       *  - secure
-       */
-      put: function(key, value, options) {
-        if (!key || /^(?:expires|max\-age|path|domain|secure)$/i.test(key)) {
-          return false;
-        }
-        var expires = "",
-          domain = (options && options.domain) || '',
-          path = (options && options.path) || '',
-          secure = (options && options.secure) || false;
+    this.put = function (name, value, options) {
+      rawDocument.cookie = encodeCookie(name, value, options);
+      var cookies = rawDocument.cookies.length + 1;
 
-        if (options && options.end) {
-          switch (options.end.constructor) {
-            case Number:
-              expires = options.end === Infinity ? "; expires=Fri, 31 Dec 9999 23:59:59 GMT" : "; max-age=" + options.end;
-              break;
-            case String:
-              expires = "; expires=" + options.end;
-              break;
-            case Date:
-              expires = "; expires=" + options.end.toUTCString();
-              break;
-          }
-        }
-        $document[0].cookie = encodeURIComponent(key) + "=" + encodeValue(value)
-          + expires
-          + (domain ? "; domain=" + domain : "")
-          + (path ? "; path=" + path : "")
-          + (secure ? "; secure" : "");
-
-        return true;
-      },
-
-      remove: function(key, options) {
-
-        if (!key || !hasItem(key)) {
-          return false;
-        }
-
-        var domain = (options && options.domain) || '',
-          path = (options && options.path) || '';
-
-        $document[0].cookie = encodeURIComponent(key) + "=; expires=Thu, 01 Jan 1970 00:00:00 GMT"
-          + ( domain ? "; domain=" + domain : "")
-          + ( path ? "; path=" + path : "");
-
-        return true;
+      // per http://www.ietf.org/rfc/rfc2109.txt browser must allow at minimum:
+      // - 300 cookies
+      // - 20 cookies per unique domain
+      // - 4096 bytes per cookie
+      if (cookieLength > 4096) {
+        console.log("Cookie '" + name +
+          "' possibly not set or overflowed because it was too large (" +
+          cookieLength + " > 4096 bytes)!");
       }
     };
-  }
-);
+
+    this.get = function (name) {
+      var cookies = this.getAll();
+      return cookies[name];
+    };
+
+    this.getAll = function () {
+      var cookieArray, cookie, i, index;
+
+      if (rawDocument.cookie !== lastCookieString) {
+        lastCookieString = rawDocument.cookie;
+        cookieArray = lastCookieString.split("; ");
+        lastCookies = {};
+
+        for (i = 0; i < cookieArray.length; i++) {
+          cookie = cookieArray[i];
+          index = cookie.indexOf('=');
+          if (index > 0) { //ignore nameless cookies
+            name = safeDecodeURIComponent(cookie.substring(0, index));
+            // the first value that is seen for a cookie is the most
+            // specific one.  values for the same cookie name that
+            // follow are for less specific paths.
+            if (lastCookies[name] === undefined) {
+              lastCookies[name] = angular.fromJson(
+                  safeDecodeURIComponent(cookie.substring(index + 1)));
+            }
+          }
+        }
+      }
+      return lastCookies;
+    };
+
+  };
+
+  CookieStoreService.$inject = ['$document'];
+
+  angular.module('ngBiscuit').service('cookieStore', CookieStoreService);
+
+}).call(null);
